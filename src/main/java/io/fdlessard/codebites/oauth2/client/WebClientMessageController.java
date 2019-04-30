dc994f1e33357d2f587ec1b896dbcf8a0feeec55
fdlessard <fdlessard@gmail.com>
2019-04-30 09:22:20 -0400

    Added batch call

--- src/main/java/io/fdlessard/codebites/oauth2/client/WebClientMessageController.java
+++ src/main/java/io/fdlessard/codebites/oauth2/client/WebClientMessageController.java
@@ -1,14 +1,22 @@
 package io.fdlessard.codebites.oauth2.client;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Slf4j
 @RestController
@@ -21,7 +29,6 @@ public class WebClientMessageController {
     }
 
     @GetMapping(value = "/WebClientMessage")
-    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public Mono<Message> getMessage() {
 
@@ -40,5 +47,40 @@ public class WebClientMessageController {
         log.info("WebClientMessageController.logException({})", e);
 
         return Mono.just(new Message(e.getMessage()));
+    }
+
+
+    @GetMapping(value = "/WebClientMessages")
+    @ResponseBody
+    public Collection<Message> getAllMessages() {
+
+        List<String> ids = IntStream.range(1, 500).boxed().map(i -> i.toString()).collect(Collectors.toList());
+
+
+        return Flux.fromIterable(ids)
+                .flatMap(id -> messageWebClient.get()
+                                .uri("/message")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .retrieve()
+                                .bodyToMono(Message.class)
+                                .flatMap(c -> buildResponse(id, c))
+                                .onErrorResume(e -> buildResponse(id, e)),
+                        256)
+                .collectMap(id -> id.getT1(), id -> id.getT2())
+                .block().values();
+    }
+
+    private Mono<Tuple2<String, Message>> buildResponse(String id, Message message) {
+        return Mono.just(Tuples.of(id, message));
+    }
+
+    private Mono<Tuple2<String, Message>> buildResponse(String id, Throwable e) {
+
+        if (e instanceof WebClientResponseException) {
+            WebClientResponseException wce = (WebClientResponseException) e;
+            return Mono.just(Tuples.of(id, new Message( wce.getResponseBodyAsString())));
+        }
+
+        return Mono.just(Tuples.of(id, new Message("not WebClientResponseException")));
     }
 }
